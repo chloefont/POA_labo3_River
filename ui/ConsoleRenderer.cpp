fc9ff40b5f77a3713cc33ec966e8fe568577d6dd
Luca <coduri.vitor@gmail.com>
2022-05-10 01:10:25 +0200

    Renderer

---
+++ ui/ConsoleRenderer.cpp
@@ -0,0 +1,29 @@
+/*
+-----------------------------------------------------------------------------------
+Nom du fichier  : ConsoleRenderer.cpp
+Auteur(s)       : lucac
+Date creation   : 10.05.2022
+Description     : <à compléter>
+Remarque(s)     : <à compléter>
+Compilateur     : Mingw-w64 g++ 8.1.0
+-----------------------------------------------------------------------------------
+
+*/
+
+#include "ConsoleRenderer.hpp"
+
+void ConsoleRenderer::drawBoat(Boat boat) {
+
+}
+
+void ConsoleRenderer::drawContainer(Container &container) {
+
+}
+
+void ConsoleRenderer::drawMenu() {
+
+}
+
+void ConsoleRenderer::showWinMessage() {
+
+}
